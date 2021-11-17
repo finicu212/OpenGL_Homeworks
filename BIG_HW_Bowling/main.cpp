a15a64d7fab966df2cc612f2e236ac89cfff1a2c
finicu212 <smurfdeplastic@gmail.com>
2021-11-17 13:18:27 +0200

    rid of plane

--- BIG_HW_Bowling/main.cpp
+++ BIG_HW_Bowling/main.cpp
@@ -15,7 +15,6 @@
 #include "Cylinder.hpp"
 #include "shader.hpp"
 #include "Sphere.hpp"
-#include "Plane.hpp"
 #include "Popice.hpp"
 
 Camera cam(glm::vec3(-5.0f, 5.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
